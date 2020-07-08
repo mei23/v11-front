ebc6740df8b26ad3e61de51b91fc2f80f86d4cac
tamaina <tamaina@hotmail.co.jp>
2020-07-08 14:49:57 +0900

    適当に互換

--- src/client/app/common/views/components/reaction-icon.vue
+++ src/client/app/common/views/components/reaction-icon.vue
@@ -1,9 +1,10 @@
 <template>
-<mk-emoji :emoji="reaction.startsWith(':') ? null : reaction" :name="reaction.startsWith(':') ? reaction.substr(1, reaction.length - 2) : null" :is-reaction="true" :customEmojis="customEmojis" :normal="true" :no-style="noStyle"/>
+<mk-emoji :emoji="reaction.startsWith(':') ? null : _reaction" :name="reaction.startsWith(':') ? reaction.substr(1, reaction.length - 2) : null" :is-reaction="true" :customEmojis="customEmojis" :normal="true" :no-style="noStyle"/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import getReactionEmoji from '../../../../../misc/get-reaction-emoji';
 export default Vue.extend({
 	props: {
 		reaction: {
@@ -20,5 +21,11 @@ export default Vue.extend({
 			default: false
 		},
 	},
+
+	computed: {
+		_reaction() {
+			return getReactionEmoji(this.reaction)
+		}
+	}
 });
 </script>

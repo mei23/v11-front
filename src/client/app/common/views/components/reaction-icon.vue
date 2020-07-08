bc204168436ebae521c11708d19dd9d558a2acc4
tamaina <tamaina@hotmail.co.jp>
2020-07-08 14:30:46 +0900

    fix custonemoji

--- src/client/app/common/views/components/reaction-icon.vue
+++ src/client/app/common/views/components/reaction-icon.vue
@@ -1,53 +1,24 @@
 <template>
-<mk-emoji :emoji="str.startsWith(':') ? null : str" :name="str.startsWith(':') ? str.substr(1, str.length - 2) : null" :is-reaction="true" :custom-emojis="customEmojis" :normal="true"/>
+<mk-emoji :emoji="reaction.startsWith(':') ? null : reaction" :name="reaction.startsWith(':') ? reaction.substr(1, reaction.length - 2) : null" :is-reaction="true" :customEmojis="customEmojis" :normal="true" :no-style="noStyle"/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import i18n from '../../../i18n';
 export default Vue.extend({
-	i18n: i18n(),
 	props: {
 		reaction: {
 			type: String,
 			required: true
 		},
-	},
-	data() {
-		return {
-			customEmojis: []
-		};
-	},
-	created() {
-		this.$root.getMeta().then(meta => {
-			if (meta && meta.emojis) this.customEmojis = meta.emojis;
-		});
-	},
-	computed: {
-		str(): any {
-			switch (this.reaction) {
-				case 'like': return '👍';
-				case 'love': return '❤';
-				case 'laugh': return '😆';
-				case 'hmm': return '🤔';
-				case 'surprise': return '😮';
-				case 'congrats': return '🎉';
-				case 'angry': return '💢';
-				case 'confused': return '😥';
-				case 'rip': return '😇';
-				case 'pudding': return (this.$store.getters.isSignedIn && this.$store.state.settings.iLikeSushi) ? '🍣' : '🍮';
-				case 'star': return '⭐';
-				default: return this.reaction;
-			}
+		customEmojis: {
+			required: false,
+			default: () => []
+		},
+		noStyle: {
+			type: Boolean,
+			required: false,
+			default: false
 		},
 	},
 });
 </script>
-
-<style lang="stylus" scoped>
-.mk-reaction-icon
-	img
-		vertical-align middle
-		width 1em
-		height 1em
-</style>
